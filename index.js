82059a59897d36ed25036e2c0914c0e751342533
zeryzez <lolmitoman@gmail.com>
2023-01-05 18:37:06 +0100

    v2

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-var question = document.querySelectorAll(".boutton")
-var image = document.querySelectorAll(".arrow")
-var answer = document.querySelectorAll(".answer")
+var buttons = document.querySelectorAll(".boutton");
+var answers = document.querySelectorAll(".answer");
+var arrows = document.querySelectorAll(".arrow");
 
+buttons.forEach((button, index) => {
+  button.addEventListener("click", event => {
+    if (answers[index].style.display === "block") {
+      answers[index].style.display = "none";
+      arrows[index].style.transform = "rotate(0deg)";
+      button.style.fontWeight = "lighter";
+    } else {
+      answers[index].style.display = "block";
+      arrows[index].style.transform = "rotate(180deg)";
+      button.style.fontWeight = "bold";
+    }
+  });
+});
 
-
-for(var i = 0; i< question.length; i++){
-        question[i].addEventListener('click', () => {
-          question[i].style.color = 'red'
-        })
-}
